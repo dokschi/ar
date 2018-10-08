0b332e081e64ccf7a77d6e8b16a1753ccd590149
dokschi <34189484+dokschi@users.noreply.github.com>
2018-10-08 23:14:11 +0200

    Update serverAR.php
    test github
    change output of server response

--- serverAR.php
+++ serverAR.php
@@ -2,7 +2,7 @@
 	// test server side application 
 	// extract one part of the json object and send it back to JS for test purposes
 	$json = json_decode(file_get_contents("php://input"));
-	$response = $json[3]->instructionText;
+	$response = $json[1]->instructionText;
 	echo $response;
 
 ?>
